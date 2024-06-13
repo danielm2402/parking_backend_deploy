f719d9656dda433bc4be7c3606f983b8354cb964
Esteban Daza <daesteban@unicauca.edu.co>
2024-06-13 00:26:18 -0500

    add end points

--- src/main/java/co/edu/unicauca/iot/parking_backend/capaControladores/ImagenController.java
+++ src/main/java/co/edu/unicauca/iot/parking_backend/capaControladores/ImagenController.java
@@ -7,11 +7,17 @@ import co.edu.unicauca.iot.parking_backend.capaAccesoADatos.models.Imagen;
 import co.edu.unicauca.iot.parking_backend.fachadaServicios.services.ImagenService;
 
 import java.io.IOException;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @RestController
@@ -22,16 +28,40 @@ public class ImagenController {
     private ImagenService imagenService;
 
     @PostMapping("/upload")
-    public ResponseEntity<String> uploadImagen(@RequestPart("imagen") MultipartFile imagen) {
+    public ResponseEntity<String> uploadImagenLocal(@RequestPart("imagen") MultipartFile imagen) {
         try {
-            Imagen imagenGuardada = imagenService.guardarImagen(imagen);
-            
-            System.out.println(imagenGuardada.getIdimagen()); 
+            Imagen imagenGuardada = imagenService.guardarImagenlocal(imagen);
+
+            System.out.println(imagenGuardada.getIdimagen());
             return new ResponseEntity<>("Imagen recibida y guardada en memoria", HttpStatus.OK);
 
         } catch (IOException e) {
-            return new ResponseEntity<>("Error al procesar la imagen: " + e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>("Error al procesar la imagen: " + e.getMessage(),
+                    HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadImagen(@RequestPart("imagen") MultipartFile imagen) {
+        try {
+            Imagen imagenGuardada = imagenService.guardarImagen(imagen);
+            return new ResponseEntity<>("Imagen guardada con ID: " + imagenGuardada.getIdimagen(), HttpStatus.OK);
+        } catch (IOException e) {
+            return new ResponseEntity<>("Error al procesar la imagen: " + e.getMessage(),
+                    HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/download/{id}")
+    public ResponseEntity<byte[]> descargarImagen(@PathVariable Integer id) {
+        Optional<Imagen> imagenOptional = imagenService.obtenerImagen(id);
+
+        if (imagenOptional.isPresent()) {
+            Imagen imagen = imagenOptional.get();
+            return ResponseEntity.ok()
+                    .body(imagen.getImagen());
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 }
--- src/main/java/co/edu/unicauca/iot/parking_backend/fachadaServicios/services/ImagenService.java
+++ src/main/java/co/edu/unicauca/iot/parking_backend/fachadaServicios/services/ImagenService.java
@@ -1,6 +1,7 @@
 package co.edu.unicauca.iot.parking_backend.fachadaServicios.services;
 
 import java.io.IOException;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,11 +16,21 @@ public class ImagenService {
     @Autowired
     private ImagenRepository imagenRepository;
 
-    public Imagen guardarImagen(MultipartFile file) throws IOException {
+    public Imagen guardarImagenlocal(MultipartFile file) throws IOException {
         byte[] imagenBytes = file.getBytes();
         Imagen imagen = new Imagen();
         imagen.setImagen(imagenBytes);
         return imagenRepository.save(imagen);
+    }
+
+    public Imagen guardarImagen(MultipartFile archivo) throws IOException {
+        Imagen imagen = new Imagen();
+        imagen.setImagen(archivo.getBytes());
+        return imagenRepository.save(imagen);
+    }
+
+    public Optional<Imagen> obtenerImagen(Integer id) {
+        return imagenRepository.findById(id);
     }
 
 }
